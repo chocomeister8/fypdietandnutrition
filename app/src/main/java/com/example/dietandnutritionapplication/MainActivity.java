eedf3d6067bd5f6622ac77676e0b9de620dded03
chocomeister <mxwlow001@mymail.sim.edu.sg>
2024-08-29 19:54:17 +0800

    added a 'not registered' clickable link for users to click, able to navigate to register page using link. register orientation fixed

--- app/src/main/java/com/example/dietandnutritionapplication/MainActivity.java
+++ app/src/main/java/com/example/dietandnutritionapplication/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.dietandnutritionapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,16 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+
+        TextView loginTextView = findViewById(R.id.noregis);
+        loginTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
