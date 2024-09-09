8654c1b833741709f5012da5c1f762f04a28becd
kitthongggg <104968739+kitthongggg@users.noreply.github.com>
2024-09-09 18:13:30 +0800

    Add files via upload
    guest bottom nav bar, landing page, appreviews

---
+++ Login.java
@@ -0,0 +1,11 @@
+package com.example.dietandnutritionapplication;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
---
+++ MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.dietandnutritionapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import com.example.dietandnutritionapplication.databinding.ActivityMainBinding;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        EdgeToEdge.enable(this);
+
+        // Initialize ViewBinding
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Set up Fragment transactions
+        replaceFragment(new LandingFragment());
+        binding.bottomNavigationView.setBackground(null);
+
+        binding.fab.setOnClickListener(v -> replaceFragment(new HomeFragment()));
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.landing:
+                    replaceFragment(new LandingFragment());
+                    break;
+                case R.id.recipe:
+                    replaceFragment(new RecipeFragment());
+                    break;
+                case R.id.meallog:
+                    replaceFragment(new Meal_LogFragment());
+                    break;
+                case R.id.reviews:
+                    replaceFragment(new AppReviewsFragment());
+                    break;
+            }
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+
+}
