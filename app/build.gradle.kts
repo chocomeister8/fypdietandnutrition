b086018bed4f456ec0e1ead4592027e12d6f33aa
chocomeister <mxwlow001@mymail.sim.edu.sg>
2024-09-09 19:24:37 +0800

    deleting tonnes of comments outside closing. fixed multiple errors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,6 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation(libs.gridlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
