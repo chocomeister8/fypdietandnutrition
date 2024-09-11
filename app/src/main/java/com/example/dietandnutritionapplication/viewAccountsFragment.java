8df4843ffe49369d267950dfb46d07f19c1bcae3
zawli <zawlinhtike1962001@gmail.com>
2024-09-11 21:24:25 +0800

    commit

---
+++ app/src/main/java/com/example/dietandnutritionapplication/viewAccountsFragment.java
@@ -0,0 +1,98 @@
+package com.example.dietandnutritionapplication;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link viewAccountsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class viewAccountsFragment extends Fragment {
+    ListView listView;
+    ArrayList<String> items;
+    ArrayAdapter<String> adapter;
+    ArrayList<Profile> profiles = new ArrayList<>();
+
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public viewAccountsFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment viewAccountsFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static viewAccountsFragment newInstance(String param1, String param2) {
+        viewAccountsFragment fragment = new viewAccountsFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        items = new ArrayList<>();
+        items.add("Apple");
+        items.add("Orange");
+        MainActivity mainActivity = (MainActivity) getActivity();
+        profiles = mainActivity.getAccountArray();
+        for(Profile profile:profiles){
+            displayProfile dp = new displayProfile(profile.getUsername(),profile.getRole());
+            items.add(dp.toString());
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_view_accounts, container, false);
+        listView = view.findViewById(R.id.listView);
+
+
+        // Correct initialization of the ArrayAdapter
+        adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, items);
+
+        // Set the adapter to the ListView
+        listView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getContext(), "Item clicked: " + parent.getItemAtPosition(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
+    }
+}
