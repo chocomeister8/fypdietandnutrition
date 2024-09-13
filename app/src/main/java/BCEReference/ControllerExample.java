784024645c7311994dc63ce031ca66326178dcd3
zawli <zawlinhtike1962001@gmail.com>
2024-09-13 21:02:18 +0800

    commit

---
+++ app/src/main/java/BCEReference/ControllerExample.java
@@ -0,0 +1,14 @@
+package BCEReference;
+
+public class ControllerExample {
+    public ControllerExample(){
+
+    }
+    public static void checkAccount(String firstname, String username, String dob, String email, String phNum, String gender, String password){
+        EntityExample entity = new EntityExample();
+
+        //blah blah blah
+
+        entity.createUserAccount(firstname,username,dob,email,phNum,gender,password);
+    }
+}
