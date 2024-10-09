1177532e799dacd83d102a4bb158cc54b00ef632
zawli <zawlinhtike1962001@gmail.com>
2024-10-09 21:05:18 +0800

    commit

--- app/src/main/java/com/example/dietandnutritionapplication/MainActivity.java
+++ app/src/main/java/com/example/dietandnutritionapplication/MainActivity.java
@@ -45,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Example of fetching user role
         FirebaseUser user = mAuth.getCurrentUser();
-        String userRole = "admin"; // Default role
+        String userRole = "guest"; // Default role
 
 
         // Initialize ViewBinding
